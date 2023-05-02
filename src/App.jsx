c000bf16e939555cbb6a5a4e77e4b0b05885bb5a
sidin <officialzedro@gmail.com>
2023-05-02 15:35:02 +0530

    todo app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import "./app.css"
+import TodoList from "./components/TodoApp"
+
+
+
+
+
+function App() {
+  return (
+   <div className="App">
+    <TodoList />
+   </div>
+   
+  )
+ 
+}
+
+export default App
